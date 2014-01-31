e3ed0ad4bbb49d6561daf10a2c4e2d7dda6ad939
Sean Ephraim <sean-ephraim@uiowa.edu>
2014-01-31 16:55:47 -0600

    Added variations_0 table to migrations.

--- application/migrations/001_init.php
+++ application/migrations/001_init.php
@@ -252,7 +252,357 @@ class Migration_Init extends CI_Migration {
     $this->dbforge->add_key('id', TRUE);
     $this->dbforge->create_table('variant_count_0', TRUE);
 
-#    $this->dbforge->create_table('variations_0', TRUE);
+    # Variations table
+    $fields = array(
+      'id' => array(
+          'type' => 'INT',
+          'constraint' => 11, 
+          'unsigned' => TRUE,
+          'null' => FALSE,
+          'auto_increment' => TRUE
+      ),
+      'variation' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'gene' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 10, 
+          'null' => TRUE,
+      ),
+      'hgvs_protein_change' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'hgvs_nucleotide_change' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'variantlocale' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'pathogenicity' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'disease' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'pubmed_id' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100, 
+          'null' => TRUE,
+      ),
+      'dbsnp' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 50, 
+          'null' => TRUE,
+      ),
+      'summary_insilico' => array(
+          'type' => 'INT',
+          'constraint' => 11, 
+          'null' => TRUE,
+      ),
+      'summary_frequency' => array(
+          'type' => 'INT',
+          'constraint' => 11, 
+          'null' => TRUE,
+      ),
+      'summary_published' => array(
+          'type' => 'INT',
+          'constraint' => 11, 
+          'null' => TRUE,
+      ),
+      'comments' => array(
+          'type' => 'LONGTEXT',
+          'null' => TRUE,
+      ),
+      'lrt_omega' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'phylop_score' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'phylop_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 5,
+          'null' => TRUE,
+      ),
+      'sift_score' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'sift_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 5,
+          'null' => TRUE,
+      ),
+      'polyphen2_score' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100,
+          'null' => TRUE,
+      ),
+      'polyphen2_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 100,
+          'null' => TRUE,
+      ),
+      'lrt_score' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'lrt_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 5,
+          'null' => TRUE,
+      ),
+      'mutationtaster_score' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'mutationtaster_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 5,
+          'null' => TRUE,
+      ),
+      'gerp_nr' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'gerp_rs' => array(
+          'type' => 'FLOAT',
+          'null' => TRUE,
+      ),
+      'gerp_pred' => array(
+          'type' => 'VARCHAR',
+          'constraint' => 5,
+          'null' => TRUE,
+      ),
+      'evs_ea_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'evs_ea_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'evs_aa_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'evs_aa_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_acb_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_acb_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_asw_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_asw_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_cdx_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_cdx_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_ceu_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_ceu_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_chb_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_chb_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_chs_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_chs_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_clm_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_clm_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_fin_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_fin_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_gbr_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_gbr_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_gih_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_gih_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_ibs_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_ibs_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_jpt_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_jpt_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_khv_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_khv_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_lwk_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_lwk_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_mxl_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_mxl_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_pel_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_pel_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_pur_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_pur_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_tsi_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_tsi_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_yri_ac' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+      'tg_yri_an' => array(
+          'type' => 'INT',
+          'constraint' => 11,
+          'null' => TRUE,
+      ),
+    );
+    $this->dbforge->add_field($fields);
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('variations_0', TRUE);
+
 #    $this->dbforge->create_table('variations_queue_0', TRUE);
 #    $this->dbforge->create_table('versions', TRUE);
   }
