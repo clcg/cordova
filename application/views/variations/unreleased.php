6205b13ca46516e1c76d7817145366baed75f189
Sean Ephraim <sean-ephraim@uiowa.edu>
2014-02-12 12:19:55 -0600

    Fixed the position of the "Special release options" tab

--- application/views/variations/unreleased.php
+++ application/views/variations/unreleased.php
@@ -56,64 +56,68 @@ echo form_open('variations/submit', $attributes);
             </label>
         </div>
     <?php endforeach; ?>
-    <?php echo $page_links; ?>
-    <!-- "Release changes" button (hide if no variant changes exist) -->
-    <div id="release-changes-wrapper" class="rounded <?php echo hidden(empty($variants)); ?>">
-        <h4>Ready to release these changes?</h4> 
-        <button id="release-variant-changes" type="button" class="btn btn-primary btn-medium" data-toggle="modal" data-target="#modal-release-confirm">Release changes</button>
-    </div>
 
-    <!-- "Confirm/Unconfirm all" buttons (hide if no variant changes exist) -->
-    <span id="confirm-toggles" class="<?php echo hidden(empty($variants)); ?>">
-        <label id="unconfirm-all" class="hold-change" type="button">
-            <i class="icon-off"></i>
-        </label>
-        <label id="confirm-all" class="release-change" type="button">
-            <i class="icon-off"></i>
-        </label>
-        <span id="confirm-all-label">Confirm/unconfirm all</span>
-    </span>
+    <?php echo $page_links; ?>
+
+    <div class="row-fluid">
+        <!-- "Release changes" button (hide if no variant changes exist) -->
+        <div id="release-changes-wrapper" class="span3 rounded <?php echo hidden(empty($variants)); ?>">
+            <h4>Ready to release these changes?</h4> 
+            <button id="release-variant-changes" type="button" class="btn btn-primary btn-medium" data-toggle="modal" data-target="#modal-release-confirm">Release changes</button>
+        </div>
+    
+        <!-- "Special release options" tab (hide if no variant changes exist) -->
+        <div id="special-release-options" class="span4 accordion <?php echo hidden(empty($variants)); ?>">
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a class="accordion-toggle rowlink" data-toggle="collapse" data-parent="#special-release-options" href="#special-release-options-collapse">
+                      <i class="icon-plus"></i> <b>Special release options</b>
+                    </a>
+                </div>
+                <div id="special-release-options-collapse" class="accordion-body collapse">
+                    <div class="accordion-inner">
+                        <p class="label-radio-pair">
+                            <input type="radio" id="special-release-none" name="special-release" value="none" checked>
+                            <label for="special-release-none">
+                                <b>None</b><br/>
+                                <small>All changes must be confirmed prior to release.</small>
+                            </label>
+                        </p>
+                        <p class="label-radio-pair">
+                            <input type="radio" id="special-release-force-confirmed" name="special-release" value="force-confirmed">
+                            <label for="special-release-force-confirmed">
+                                <b>Force confirmed only</b><br/>
+                                <small>Release only the changes that have been confirmed. All other changes will remain in the queue.</small>
+                            </label>
+                        </p>
+                        <p class="label-radio-pair">
+                            <input type="radio" id="special-release-force-all" name="special-release" value="force-all">
+                            <label for="special-release-force-all">
+                              <b>Force all</b><br/>
+                              <small>Release all changes regardless of confirmation status.</small>
+                            </label>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <!-- "Confirm/Unconfirm all" buttons (hide if no variant changes exist) -->
+        <span id="confirm-toggles" class="span4 offset1 <?php echo hidden(empty($variants)); ?>">
+            <label id="unconfirm-all" class="hold-change" type="button">
+                <i class="icon-off"></i>
+            </label>
+            <label id="confirm-all" class="release-change" type="button">
+                <i class="icon-off"></i>
+            </label>
+            <span id="confirm-all-label">Confirm/unconfirm all</span>
+        </span>
+    </div>
 
     <!-- "Initial release" button (display for Version 0 only) -->
     <div id="initial-release-wrapper" class="rounded <?php echo hidden(($this->version != 0)); ?>">
         <h4>Create an initial release?</h4> 
         <button name="release-changes" class="btn btn-medium btn-primary" type="submit" value="Release">Create</button>
-    </div>
-
-    <!-- "Special release options" tab (hide if no variant changes exist) -->
-    <div class="accordion <?php echo hidden(empty($variants)); ?>" id="special-release-options">
-        <div class="accordion-group">
-            <div class="accordion-heading">
-                <a class="accordion-toggle rowlink" data-toggle="collapse" data-parent="#special-release-options" href="#special-release-options-collapse">
-                  <i class="icon-plus"></i> <b>Special release options</b>
-                </a>
-            </div>
-            <div id="special-release-options-collapse" class="accordion-body collapse">
-                <div class="accordion-inner">
-                    <p class="label-radio-pair">
-                        <input type="radio" id="special-release-none" name="special-release" value="none" checked>
-                        <label for="special-release-none">
-                            <b>None</b><br/>
-                            <small>All changes must be confirmed prior to release.</small>
-                        </label>
-                    </p>
-                    <p class="label-radio-pair">
-                        <input type="radio" id="special-release-force-confirmed" name="special-release" value="force-confirmed">
-                        <label for="special-release-force-confirmed">
-                            <b>Force confirmed only</b><br/>
-                            <small>Release only the changes that have been confirmed. All other changes will remain in the queue.</small>
-                        </label>
-                    </p>
-                    <p class="label-radio-pair">
-                        <input type="radio" id="special-release-force-all" name="special-release" value="force-all">
-                        <label for="special-release-force-all">
-                          <b>Force all</b><br/>
-                          <small>Release all changes regardless of confirmation status.</small>
-                        </label>
-                    </p>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- "Release changes" confirmation modal -->
@@ -130,6 +134,7 @@ echo form_open('variations/submit', $attributes);
             <button name="release-changes" class="btn btn-medium btn-primary" type="submit" value="Release">OK</button>
         </div>
     </div>
+
     <!-- "Save changes" button -->
     <div id="affixed-save-wrapper" class="affix" data-offset-top="50">
         <button name="save-changes" class="btn btn-medium btn-primary" type="submit" value="Save">Save</button>
