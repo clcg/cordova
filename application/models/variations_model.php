7cecb556e7a27c0a9bd3f11246257ca4aa61c4e2
lucmurray <lucas.murray2014@gmail.com>
2015-03-25 21:35:41 -0500

    Update variations_model.php
    Added links to 3D structures

--- application/models/variations_model.php
+++ application/models/variations_model.php
@@ -1342,6 +1342,7 @@ EOF;
    *
    * @author Nikhil Anand
    * @author Zachary Ladlie
+   * @author Lucas Murray
    * @access public
    * @param string $result 
    * 			An array of database results for a gene letter
@@ -1371,7 +1372,7 @@ EOF;
       $table .=<<<EOF
       \n
       <fieldset>
-          <legend class="genename $collapsed" id="$gene"><strong>$gene</strong> <span><a href="$uri_csv">CSV</a> <a href="$uri_tab">Tab</a> <a href="$uri_jsn">JSON</a> <a href="$uri_xml">XML</a></span></legend>
+          <legend class="genename $collapsed" id="$gene"><strong>$gene</strong> <span><a href="$uri_csv">CSV</a> <a href="$uri_tab">Tab</a> <a href="$uri_jsn">JSON</a> <a href="$uri_xml">XML</a><a href="https://github.com/wtollefson/dvd-structures/tree/master/$gene">3D Structure</a></span></legend>
           <div id="table-$gene" style="$display">
               <table class="gene-table">
               <thead>
