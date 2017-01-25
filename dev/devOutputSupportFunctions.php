a68f85edd717ea21154eb944bf260a953b413d3c
rmarinivision <rmarinivision@192.168.0.4>
2017-01-25 11:35:29 -0600

    url position try 2: attempt 11
    attempting to create mock console by writing to a file in new dev folder
    modifications made to variations.php, devOutpuSupportfunctions, and
    deletion of developmentOutput.php file

--- dev/devOutputSupportFunctions.php
+++ dev/devOutputSupportFunctions.php
@@ -3,9 +3,11 @@ class devStuff {
 	
 	public $devOutputStrings = array();
 	
-	public function devOutput() {
+	public function printToDevOutputFile($content) {
 	
-	
+		$file = './dev/MockConsoleOutput.txt';
+		
+		file_put_contents($file, $content);
 		
 	}
 }
