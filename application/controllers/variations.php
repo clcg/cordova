a4d8b2c2ceaf7dfa649f5875498307a90d63755d
Rob-pc <Rob-pc@192.168.0.7>
2017-01-31 11:52:39 -0600

    url position: try3:
    same as previous

--- application/controllers/variations.php
+++ application/controllers/variations.php
@@ -581,6 +581,7 @@ class Variations extends MY_Controller {
   	
   	foreach ($variants as $aVariant) {
   		$aVariant = json_decode(json_encode($aVariant),True); //this should convert the stdObject type to an array type
+  		$this->printToScreen($aVariant);
   		if(strpos($aVariant['variant'], $positionAndAllele['allele']) !== false) {
   			$variant = $aVariant;
   			break;
