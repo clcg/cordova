0e4e09909215614445b868524072ca7582d0e1ae
Rob-pc <Rob-pc@192.168.0.7>
2017-01-31 09:43:29 -0600

    url position try3:
    another shot at Sean's output method

--- application/controllers/variations.php
+++ application/controllers/variations.php
@@ -576,6 +576,7 @@ class Variations extends MY_Controller {
   
   	//$data = $this->variations_model->get_variant_display_variables($id, $this->tables['vd_live']);
   	$positionFormatted = $this->format_position_from_url_safe($position);
+  	$this->load->model('variations_model');
   	$variant = $this->variations_model->get_variants_by_position("chr13:20763686");
   	$this->printToScreen($variant);
   	
