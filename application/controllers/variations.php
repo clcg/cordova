0f9bcea3d9be8067ed901b40a3322162837fb5ee
Rob-pc <Rob-pc@192.168.0.7>
2017-01-31 12:33:08 -0600

    url position: try3:
    worked as whole for:
    chr10_89623197_T>G
    
    editing to work for others:
    chr13_20763686_C>A
    chr13_20763686_C>-
    chr13_20763686_->C

--- application/controllers/variations.php
+++ application/controllers/variations.php
@@ -577,6 +577,7 @@ class Variations extends MY_Controller {
   	//$data = $this->variations_model->get_variant_display_variables($id, $this->tables['vd_live']);
   	$positionAndAllele = $this->format_position_from_url_safe($positionUrlSafe);
   	$variants = $this->variations_model->get_variants_by_position($positionAndAllele['position']); //'chr10:89623197'
+  	$this->printToScreen($variants);
 
   	
   	foreach ($variants as $aVariant) {
