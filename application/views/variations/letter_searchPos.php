21db4f55a7670329d6570cc9574f4db7c3890a8e
rmarini <rmarini@uiowa.edu>
2017-02-15 09:01:13 -0600

    search bar pos: try3:
    same as previous

--- application/views/variations/letter_searchPos.php
+++ application/views/variations/letter_searchPos.php
@@ -3,7 +3,7 @@
     <fieldset>
         <legend class="genename" id="<?php echo $gene ?>"><strong><?php echo $display_names[$gene] ?></strong> <span><a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> <a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> <a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> <a href="<?php echo gene_link_to_api($gene, 'xml') ?>">XML</a></span></legend>
         <div id="table-<?php echo $gene ?>" class="variant-list-container">
-        	<?php $geneTable?>
+        	<?php $this->load->view($geneTable)?>
         </div>
     </fieldset>
     <?php endforeach; ?>
