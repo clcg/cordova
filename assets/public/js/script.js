60bb959835146e2cd636caf2202a0647036bb597
rmarini <rmarini@uiowa.edu>
2017-03-29 14:06:58 -0500

    trouble linking along the route, trying to fix...

--- assets/public/js/script.js
+++ assets/public/js/script.js
@@ -92,6 +92,7 @@ $(document).ready(function(){
 //          var temp = parts[i].split("=");
 //          $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
 //      }
+    	  alert(parts[1]);
     	  var loadURL = "../geneVariantPos/" + parts[1]; //providing the position searched here
 	  //end of Rob Marini Edits (includes this if-structure. Originally only the else clause existed without the if-structure
       } else {	  
