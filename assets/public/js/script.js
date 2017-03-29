794569f049f13180f499e73d9c5d4bf3969e3948
rmarini <rmarini@uiowa.edu>
2017-03-29 14:24:55 -0500

    hmmmm?

--- assets/public/js/script.js
+++ assets/public/js/script.js
@@ -92,8 +92,8 @@ $(document).ready(function(){
 //          var temp = parts[i].split("=");
 //          $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
 //      }
-    	  var loadURL = "../geneVariantPos/" + parts[1]; //providing the position searched here
-    	  loadURL = "../gene/" + parts[1];
+//    	  var loadURL = "../geneVariantPos/" + parts[1]; //providing the position searched here
+    	  var loadURL = "../gene/"+parts[1];
     	  //end of Rob Marini Edits (includes this if-structure. Originally only the else clause existed without the if-structure
       } else {	  
 	      var loadURL = "../gene/"+this.id; //this.id is the gene name
