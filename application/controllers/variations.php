cbb9e862ad83443df1c342387cd275c3a2ccc747
rmarini <rmarini@uiowa.edu>
2017-03-29 22:56:51 -0500

    another shot for home

--- application/controllers/variations.php
+++ application/controllers/variations.php
@@ -558,7 +558,7 @@ class Variations extends MY_Controller {
 //   	$this->printToScreen($variants);
 
   	
-  	$data['title'] = $positionAndAllel['position'];
+  	$data['title'] = $positionAndAllele['position'];
   	$data['content'] = 'variations/gene';
   
   	$data['gene'] = $variants[0]->gene;
@@ -589,7 +589,7 @@ class Variations extends MY_Controller {
 //   	$columns = 'id,hgvs_protein_change,hgvs_nucleotide_change,variantlocale,variation,pathogenicity,disease';
 //   	$data['variations'] = $this->variations_model->get_variants_by_gene($gene, $columns);
   
-  	$this->printToScreen($data['variations']);
+//   	$this->printToScreen($data['variations']);
   
   	$this->load->view('variations/gene', $data);
   }
