d6f35f19dfe43cdb5cb4ddeae33473cfe8cc787f
rmarini <rmarini@uiowa.edu>
2017-04-19 00:06:49 -0500

    same as previous

--- application/views/variations/letter_searchPos.php
+++ application/views/variations/letter_searchPos.php
@@ -1,7 +1,7 @@
 <div id="mutation-tables">
     <?php foreach ($genes as $gene => $alias): ?>
     <fieldset>
-        <legend class="genename" id="<?php echo $gene ?>"><strong><?php echo $display_names[$gene] ?></strong> <span><a href="viewer/"$gene>Protein Viewer</a><a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> <a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> <a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> <a href="<?php echo gene_link_to_api($gene, 'xml') ?>">XML</a></span></legend>
+        <legend class="genename" id="<?php echo $gene ?>"><strong><?php echo $display_names[$gene] ?></strong> <span><a href="viewer/"+$gene>Protein Viewer</a><a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> <a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> <a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> <a href="<?php echo gene_link_to_api($gene, 'xml') ?>">XML</a></span></legend>
         <div id="table-<?php echo $gene ?>" class="variant-list-container">
         	<fieldset>
 		        <table class="gene-table">
