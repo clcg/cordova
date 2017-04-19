8727ebf918beaceed9ade4900cc152401f4efb1f
rmarini <rmarini@uiowa.edu>
2017-04-19 01:35:13 -0500

    I think I have it...But my db doesn't have any PV genes...
    Need to get a full db dump on my site

--- application/views/variations/letter.php
+++ application/views/variations/letter.php
@@ -5,7 +5,7 @@
         	<strong><?php echo $display_names[$gene] ?></strong> 
         	<span>
         	<!-- <a href="<?php echo site_url('controller/function/uri') ?>">Link</a> -->
-        		<a href="<?php echo site_url('viewer/'+$gene)?>">Protein Viewer</a>
+        		<a href="<?php echo site_url('variations/viewer/'+$gene)?>">Protein Viewer</a>
         		<a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> 
