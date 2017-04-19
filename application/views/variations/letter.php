49735385d7c7f7d04b2ac827b9ca7d4e1a936f9e
rmarini <rmarini@uiowa.edu>
2017-04-19 00:56:51 -0500

    same as previous

--- application/views/variations/letter.php
+++ application/views/variations/letter.php
@@ -4,7 +4,8 @@
         <legend class="genename" id="<?php echo $gene ?>">
         	<strong><?php echo $display_names[$gene] ?></strong> 
         	<span>
-        		<a href="<?php echo viewer($gene)?>">Protein Viewer</a>
+        	<a href="<?php echo site_url('controller/function/uri') ?>">Link</a>
+        		<a href="<?php echo site_url('variations/viwer/$gene')?>">Protein Viewer</a>
         		<a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> 
