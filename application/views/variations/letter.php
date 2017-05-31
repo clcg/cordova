c2d7e6ac89d16fc6c323f3f914e1b12f4bfd8790
rmarini <rmarini@uiowa.edu>
2017-05-30 23:44:20 -0500

    Added dynamically generated PV link to letter.php page

--- application/views/variations/letter.php
+++ application/views/variations/letter.php
@@ -5,7 +5,8 @@
         	<strong><?php echo $display_names[$gene] ?></strong> 
         	<span>
         	<!-- <a href="<?php echo site_url('controller/function/uri') ?>">Link</a> -->
-        		<a href="<?php echo site_url('variations/viewer/'+$gene)?>">Protein Viewer</a>
+        		<?php if(is_dir("assets/public/pdb/dvd_structures/$gene")){ ?><a href="<?php echo site_url('variations/viewer/'.$gene); ?>">Protein Viewer</a><?php } ?> <!-- originally its viewer/ instead of variations/viewer -->
+        		<!-- <a href="<?php echo site_url('variations/viewer/'+$gene)?>">Protein Viewer</a> -->
         		<a href="<?php echo gene_link_to_api($gene, 'csv') ?>">CSV</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'tab') ?>">Tab</a> 
         		<a href="<?php echo gene_link_to_api($gene, 'json') ?>">JSON</a> 
